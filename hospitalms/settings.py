6f62da9eb8d14b1e1e8796a0ad46cba537c0ed47
Nusaiba E <nusaiba@gmail.com>
2026-06-18 18:27:30 +0400

    static files issue

--- hospitalms/settings.py
+++ hospitalms/settings.py
@@ -34,9 +34,12 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
 ]
 
 ROOT_URLCONF = 'hospitalms.urls'
+
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 TEMPLATES = [
     {
